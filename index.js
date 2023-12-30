8137f1a2f35f4c5397c3fd09bdd7c478cb02219d
khaledmraad <khaled.mrad@ensi-uma.tn>
2023-12-30 02:55:45 +0100

    Simple ws messages

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ var s = new server({ port: 8888 });
 s.on("connection", function (ws) {
   ws.on("message", function (message) {
     console.log("received " + message);
-    if (message == "hello") {
-      ws.send("hello from the other side");
-    }
+
+    //message broadcast
+    s.clients.forEach(function e(client) {
+      if (client != ws) {
+        client.send(message.toString());
+      }
+    });
+
+    //ws.send(message.toString());
   });
+
+  ws.on("close", function () {
+    console.log("press F5 for respect");
+  });
+
+  console.log("nvm his back");
 });
